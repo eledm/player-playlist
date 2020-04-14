e354ac3706bf11b32f54af085c2e8cfda0676538
eledm <elena@MacBook-Pro-de-Elena.local>
2020-04-14 19:20:25 +0200

    Added List and Audio Source

---
+++ Player-playlist/Assets/Scripts/AudioItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AudioItem
+{
+    public string name;
+    public AudioClip myClip;
+    
+    //public Volume vol;
+    //public Panning pan;
+    //public Pitch pitch;
+
+    public AudioItem(AudioClip aClip)
+    {
+        this.myClip = aClip;
+    }
+	
+}
+
+/*public class Volume : AudioItem
+{
+	
+}*/
---
+++ Player-playlist/Assets/Scripts/PlaylistManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaylistManager : MonoBehaviour
+{
+    AudioSource _myAudioSource;
+    //create an enum with different states: fade, no fade, continuous
+    public enum FadeType
+    {
+        Fade,
+        NoFade,
+        Continuous
+    }
+
+    public FadeType currentFadeType;
+
+    //list of AudioItems
+	public List<AudioItem> audioPlaylist = new List<AudioItem>();
+
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        _myAudioSource.clip = audioPlaylist[0].myClip;
+        _myAudioSource.Play();
+
+    }
+
+    // Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    
+
+    
+}
