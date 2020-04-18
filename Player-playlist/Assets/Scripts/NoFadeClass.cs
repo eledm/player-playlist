d98e43b95bc8ab9729f199fa923865f99ee2c838
eledm <elena@MacBook-Pro-de-Elena.local>
2020-04-18 13:51:21 +0200

    Loop works on Fade
    I added the loop option on Fade, tried to move Fade to another script but it doesn't work yet, need to figure out how to connect both scripts with an Instance.
    I also started FadeIn and it works.

---
+++ Player-playlist/Assets/Scripts/NoFadeClass.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoFadeClass : PlaylistManager
+{
+    public static NoFadeClass Instance;
+
+    private void Awake()
+    {
+
+        Instance = this;
+    }
+
+    public void NoFadePlay()
+    {
+        StartCoroutine(PlayNoFade());
+    }
+    
+    private IEnumerator PlayNoFade()
+    {
+        
+        while (isLooping == true)
+        {
+            for (int i = 0; i < audioPlaylist.Count; i++)
+            {
+                myAudioSource.clip = audioPlaylist[i].myClip;
+                myAudioSource.Play();
+                yield return new WaitForSeconds(myAudioSource.clip.length);
+                Debug.Log(audioPlaylist.Count);
+
+
+                if (i == audioPlaylist.Count)
+                    i = 0;
+
+            }
+        }
+
+
+        foreach (AudioItem audio in audioPlaylist)
+        {
+            myAudioSource.clip = audio.myClip;
+            myAudioSource.Play();
+            yield return new WaitForSeconds(myAudioSource.clip.length);
+
+        }
+
+    }
+}
--- Player-playlist/Assets/Scripts/PlaylistManager.cs
+++ Player-playlist/Assets/Scripts/PlaylistManager.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Audio;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
@@ -33,10 +35,11 @@ public class PlaylistManager : MonoBehaviour
         switch (currentFadeType)
 		{
             case FadeType.NoFade:
-                StartCoroutine(NoFade());
+                NoFadeClass.Instance.NoFadePlay();
                 break;
 
             case FadeType.Fade:
+                StartCoroutine(FadeIn(0.01f));
                 break;
 
             case FadeType.Continuous:
@@ -51,9 +54,28 @@ public class PlaylistManager : MonoBehaviour
 
 	}
 
-    
-	IEnumerator NoFade ()
+    //COROUTINES
+
+    /*IEnumerator PlayNoFade()
     {
+
+        while (isLooping == true)
+        {
+            for (int i = 0; i < audioPlaylist.Count; i++)
+            {
+                myAudioSource.clip = audioPlaylist[i].myClip;
+                myAudioSource.Play();
+                yield return new WaitForSeconds(myAudioSource.clip.length);
+                Debug.Log(audioPlaylist.Count);
+
+
+                if (i == audioPlaylist.Count)
+                    i = 0;
+
+            }
+        }
+
+
         foreach (AudioItem audio in audioPlaylist)
         {
             myAudioSource.clip = audio.myClip;
@@ -61,6 +83,27 @@ public class PlaylistManager : MonoBehaviour
             yield return new WaitForSeconds(myAudioSource.clip.length);
 
         }
+
+    }***/
+
+    private float _minVolume = 0f;
+    private float _maxVolume = 1f;
+
+    IEnumerator FadeIn(float speed)
+    {
+
+        myAudioSource.clip = audioPlaylist[0].myClip;
+        myAudioSource.volume = _minVolume;
+        float audioVolume = myAudioSource.volume;
+        myAudioSource.Play();
+
+        while (myAudioSource.volume < _maxVolume)
+        {
+            audioVolume += speed;
+            myAudioSource.volume = audioVolume;
+            yield return new WaitForSeconds(0.1F);
+        }
+
     }
 
 }
